7850d410b35f9b6fbec3c861a682c6eddf0feb3b
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-26 13:06:55 -0700

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true})
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.get("/", (req, res) => res.send("trying different text"));
