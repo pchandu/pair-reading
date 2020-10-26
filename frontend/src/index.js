0a4880b7c68f68c1d6c1efb88a14599e7b8717c5
kevinsu <kevin.suboy@gmail.com>
2020-10-26 14:21:55 -0700

    added missing doc EventListener

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root')
+  ReactDOM.render(
+    <h1>Hello World</h1>, root
+  );
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
