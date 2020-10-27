8a72579bb97daaa0c8f01601510fe6972bacbaa1
Kathy Chan <kathykemopaw@gmail.com>
2020-10-27 10:24:20 -0700

    Work on Nav-Bar Componenet

---
+++ frontend/src/components/nav/navbar_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+// import { logout } from "../../actions/session_actions";
+
+import NavBar from "./navbar";
+
+const mapStateToProps = (state) => ({
+    loggedIn: state.session.isAuthenticated
+});
+
+const mapDispatchToProps = dispatch => {
+    
+};
+
+export default connect(mapStateToProps, mapDispatchToProps )(NavBar);
