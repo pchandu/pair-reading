f7f788f88b611c329cea2a9a95a7cc07d4322e16
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-27 14:22:23 -0700

    created a base css file and starting styling preferences checklist

--- frontend/src/components/onboarding/onboarding.js
+++ frontend/src/components/onboarding/onboarding.js
@@ -4,6 +4,11 @@ import React from 'react';
 class Onboarding extends React.Component {
     // constructor(props){
     //     super(this)
+    //     this.handleSubmit = this.handleSubmit.bind(this)
+    // }
+
+    // handleSubmit(){
+
     // }
 
     render(){
@@ -12,20 +17,24 @@ class Onboarding extends React.Component {
                 <ul className="preferences-container"> 
                     <li className="preferences">
                         <input type="checkbox" id="preference-morning" name="preference-morning" />
-                        <p>Morning (8am-12pm)</p>
+                        <p className="preferences-time-of-day">Morning</p>
+                        <p className="preferences-timing">(8am-12pm)</p>
                     </li>
                     <li className="preferences">
-                        <input type="checkbox" id="preference-morning" name="preference-morning" />
-                        <p>Afternoon (12pm-4pm)</p>
+                        <input type="checkbox" id="preference-afternoon" name="preference-afternoon" />
+                        <p className="preferences-time-of-day">Afternoon </p>
+                        <p className="preferences-timing">(12pm-4pm)</p>
                     </li>
                     <li className="preferences">
-                        <input type="checkbox" id="preference-morning" name="preference-morning" />
-                        <p>Evening (5pm-9pm)</p>
+                        <input type="checkbox" id="preference-evening" name="preference-evening" />
+                        <p className="preferences-time-of-day">Evening</p>
+                        <p className="preferences-timing">(5pm-9pm)</p>
                     </li>
                 </ul>
 
                 <div className="books-container">
                     <ul>
+                        
                     </ul>
                 </div>
             </div>
