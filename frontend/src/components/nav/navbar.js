cf78d367b622438a3fbd44d1e2ad211f00d6b1dd
Kathy Chan <kathykemopaw@gmail.com>
2020-10-27 13:52:05 -0700

    Add Logo and Nav Bar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,11 +1,19 @@
 import React from "react";
+import logo from "../../PR-Logo.png";
 // import { Link } from "react-router-dom";
 
 
 class NavBar extends React.Component {
+
     render(){
         return(
-            <h1>Sephora is a great site</h1>
+            <div>
+                <nav className="navbar navbar-expand-lg nav-bg">
+                    <a className="navbar-brand" href="#">
+                         <img src={logo} width="auto" height="50" className="d-inline-block align-top nav-bg" alt="Pair Reading Logo" />
+                    </a>
+                </nav>
+            </div>
         )
     }
 };
