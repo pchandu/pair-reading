2619aca980b954d2c1e01d3a17ee93a44ed207f3
AlexArchibeque <wonkman33@gmail.com>
2020-10-27 10:49:55 -0700

    FrontEnd Skele Made

---
+++ frontend/src/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Switch } from 'react-router-dom';
+
+const App = () => {
+
+    return (
+
+        <div>
+            <h1>This is where the Routes/Components Go!</h1>
+        </div>
+    )
+}
+
+
+
+export default App;
