47b900b67035bf52f09045c1015d041ade22812d
Kathy Chan <kathykemopaw@gmail.com>
2020-10-27 10:56:39 -0700

    Change files for testing backend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch } from "react-router-dom";
+// import { AuthRoute, ProtectedRoute } from "../util/route_util";
+// import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 
 // import TweetsContainer from "./tweets/tweets_container";
