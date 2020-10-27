b245a91e351e108f5120b1b1336b0798646f21ad
Kathy Chan <kathykemopaw@gmail.com>
2020-10-27 10:31:51 -0700

    Add files for Nav-Bar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 
 
 class NavBar extends React.Component {
-    
+    render(){
+        return(
+            <h1>Sephora is a great site</h1>
+        )
+    }
 };
 
 export default NavBar;
