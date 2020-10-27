910fd02c985451bb55dc13b09eb638eebd238297
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-27 10:08:51 -0700

    models & validation

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const passport = require('passport');
+require('./config/passport')(passport);
 
 mongoose
-    .connect(db, { useNewUrlParser: true})
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+.connect(db, { useNewUrlParser: true})
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-app.get("/", (req, res) => res.send("trying different text"));
+app.use(passport.initialize());
+app.use("/api/users", users);
+
