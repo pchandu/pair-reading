f3c5ffbb5738a5ddc20823bdfe287a55de4cdfb3
kevinsu <kevin.suboy@gmail.com>
2020-10-27 23:04:19 -0700

    new routes added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const books = require("./routes/api/books")
+const forums = require("./routes/api/forums")
+// const posts = require("./routes/api/posts")
+// const bookclubs = require("./routes/api/bookclubs")
 const passport = require('passport');
 require('./config/passport')(passport);
 
@@ -21,8 +24,14 @@ app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use(passport.initialize());
+
+//! START ROUTES
 app.use("/api/users", users);
 app.use("/api/books", books);
+app.use("/api/forums", forums);
+// app.use("/api/posts", posts);
+// app.use("/api/bookclubs", bookclubs);
+//! END ROUTES
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
