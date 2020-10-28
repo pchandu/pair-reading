082c56cbe00c9b79f832ff89158ec9fa6c81002f
kevinsu <kevin.suboy@gmail.com>
2020-10-28 09:35:39 -0700

    properly salted pwords

--- seeds/user-seeder.js
+++ seeds/user-seeder.js
@@ -1,11 +1,14 @@
 // const exit = require('./entry')
 
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 
 module.exports = (nextSeeder) => {
 User.deleteMany({}, () => {
     console.log('Deleted all Users')
 })
+
 
 const users = [
     new User({
@@ -31,15 +34,22 @@ const users = [
 ];
 let done = 0;
 for(let i=0;i<users.length;i++){
-    users[i].save(
-    (err,res)=>{
-        done++;
-        if(done===users.length){
-            // exit();
-            // return true;
-            nextSeeder();
-        }
-    });
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(users[i].password, salt, (err, hash) => {
+            if (err) throw err;
+            users[i].password = hash;
+            users[i].save(
+            (err, res) => {
+                done++;
+                if (done === users.length) {
+                    // exit();
+                    // return true;
+                    nextSeeder();
+                }
+            })
+        })
+    })
+
     console.log(users[i]._id);
 }
 }
