6ccf30f46b4ce05e4cf808fe176cd4445cca1adc
kevinsu <kevin.suboy@gmail.com>
2020-10-28 16:42:27 -0700

    pre rebase

---
+++ frontend/src/reducers/ui/filters/forums_filters_reducer.js
@@ -0,0 +1,23 @@
+import { UPDATE_FORUMS_FILTER, CLEAR_FORUMS_FILTER } from '../actions/filter_actions';
+
+const defaultFilters = Object.freeze({
+    keywords: null,
+    postCnt: null
+});
+
+const forumsFilterReducer = (state = defaultFilters, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case UPDATE_FORUMS_FILTER:
+            const newFilter = {
+                [action.filter]: action.value
+            };
+            return Object.assign({}, state, newFilter);
+        case CLEAR_FORUMS_FILTER:
+            return defaultFilters;
+        default:
+            return state;
+    }
+};
+
+export default routesFilterReducer;
