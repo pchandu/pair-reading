e0fd723112bcec63549c0a4202be899cf6792016
AlexArchibeque <wonkman33@gmail.com>
2020-10-30 09:42:42 -0700

    Stuff

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -9,66 +9,19 @@ class DashBoard extends React.Component {
         return(
             <div className="outer-dashboard-container">
                 <div className="dashboard-content-container">
+                    
                     <div className="left-side-dashboard-container">
                         <ProfileContainer />
                     </div>
 
                     <div className="middle-side-dashboard-container">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                      culpa qui officia deserunt mollit anim id est laborum.
 
                     </div>
 
                     <div className="right-side-dashboard-container">
                         <Calendar />
                     </div>
+
                 </div>
             </div>
         )
