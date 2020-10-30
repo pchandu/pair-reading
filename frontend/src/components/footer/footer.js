d1929e491602c2806e2fbac166492c66167130e6
AlexArchibeque <wonkman33@gmail.com>
2020-10-29 17:49:39 -0700

    Finally have footer at the bottom

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Footer extends React.Component {
+
+    render() {
+        return(
+            <div className="footer-outside-container">
+                <hr/>
+                <h1>This Be the footer.</h1>
+            </div>
+        )
+    }
+}
+
+export default Footer;
