38e6079bda8d0baecbe6c87c062a77d62b42183f
Kathy Chan <kathykemopaw@gmail.com>
2020-10-29 17:26:07 -0700

    Small changes

--- frontend/src/components/dashboard/calendar/calendar.js
+++ frontend/src/components/dashboard/calendar/calendar.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { gapi } from 'gapi-script'
+// import { gapi } from 'gapi-script'
+
 
 class Calendar extends React.Component {
-  // let gapi = window.gapi
-  // let CLIENT_ID = "457315715399-qqcemqrn9d954e43jrghmma23gc2oj32.apps.googleusercontent.com";
-  // let API_KEY = "AIzaSyBHzyqm3L4KNuX3RySWfEqaEfuXExm-u3U";
-  // let CLIENT_SECRET = "fXaqdNQQaavw3qgiw4mUBVK";
+  // var gapi = window.gapi;
+
   // let DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
   // let SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
@@ -24,12 +23,12 @@ class Calendar extends React.Component {
 
   //       gapi.auth2.getAuthInstance().signIn()
   //     })
-  // }onClick={startCalendar}
+  // }
 
   render() {
     return (
       <div>
-        <button>Go to Google Calendar</button>
+        <button >Go to Google Calendar</button>
       </div>
     );
   }
