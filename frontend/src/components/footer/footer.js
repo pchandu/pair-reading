f3095087ddcd5a2bddad6e44a64c77d74740b961
AlexArchibeque <wonkman33@gmail.com>
2020-10-29 20:42:14 -0700

    Footer stays at the bottom

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -5,7 +5,6 @@ class Footer extends React.Component {
     render() {
         return(
             <div className="footer-outside-container">
-                <hr/>
                 <h1>This Be the footer.</h1>
             </div>
         )
