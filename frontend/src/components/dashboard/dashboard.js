857e99869305cf9fee005b522d5850402d12f572
Kathy Chan <kathykemopaw@gmail.com>
2020-10-30 10:16:43 -0700

    Change dashboard.js

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,32 +1,25 @@
-import React from 'react'
-import ProfileContainer from '../profile/profile_container'
-import Calendar from './calendar/calendar'
-
+import React from "react";
+import ProfileContainer from "../profile/profile_container";
+import Calendar from "./calendar/calendar";
 
 class DashBoard extends React.Component {
-
-    render(){
-        return(
-            <div className="outer-dashboard-container">
-                <div className="dashboard-content-container">
-                    
-                    <div className="left-side-dashboard-container">
-                        <ProfileContainer />
-                    </div>
-
-                    <div className="middle-side-dashboard-container">
-
-                    </div>
-
-                    <div className="right-side-dashboard-container">
-                        <Calendar />
-                    </div>
-          
-                </div>
-            </div>
+  render() {
+    return (
+      <div className="outer-dashboard-container">
+        <div className="dashboard-content-container">
+          <div className="left-side-dashboard-container">
+            <ProfileContainer />
           </div>
-        );
-    }
+
+          <div className="middle-side-dashboard-container"></div>
+
+          <div className="right-side-dashboard-container">
+            <Calendar />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DashBoard;
