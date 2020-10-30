5367ecf3e2e7ad102a87573f0f342b3cb92594a9
AlexArchibeque <wonkman33@gmail.com>
2020-10-30 09:42:00 -0700

    Dashboard stuff

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -7,23 +7,69 @@ class DashBoard extends React.Component {
 
     render(){
         return(
-            <div className="container outer-dashboard-container">
-                Outer Top
-                <div className="row dashboard-content-container">
-                        <Calendar />
-                    <div className="col left-side-dashboard-container">
+            <div className="outer-dashboard-container">
+                <div className="dashboard-content-container">
+                    <div className="left-side-dashboard-container">
                         <ProfileContainer />
                     </div>
 
-                    <div className="col middle-side-dashboard-container">
-                        Middle Side
+                    <div className="middle-side-dashboard-container">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                      culpa qui officia deserunt mollit anim id est laborum.
+
                     </div>
 
-                    <div className="col right-side-dashboard-container">
-                        Right Side
+                    <div className="right-side-dashboard-container">
+                        <Calendar />
                     </div>
                 </div>
-                Outer Bottom
             </div>
         )
     }
