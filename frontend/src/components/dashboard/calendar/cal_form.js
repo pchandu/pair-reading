6c6bfb9d5a0a7f621646b45ed8c13cac4053123e
Kathy Chan <kathykemopaw@gmail.com>
2020-10-29 22:04:44 -0700

    Input form for event can be filled out

--- frontend/src/components/dashboard/calendar/cal_form.js
+++ frontend/src/components/dashboard/calendar/cal_form.js
@@ -11,28 +11,28 @@ class CalendarForm extends React.Component {
                 <div className="form-group row">
                     <label for="example-text-input" className="col-2 col-form-label cal-form-label">Event Title</label>
                     <div className="col-10">
-                        <input className="form-control" type="text" value="What do you want your reading session to be called?" id="example-text-input"/>
+                        <input className="form-control" type="text" placeholder="What do you want your reading session to be called?" />
                     </div>
                 </div>
 
                 <div className="form-group row">
                     <label for="example-date-input" className="col-2 col-form-label cal-form-label">Date</label>
                     <div className="col-10">
-                        <input className="form-control" type="date" value="2011-08-19" id="example-date-input"/>
+                        <input className="form-control" type="date"  />
                     </div>
                 </div>
 
                 <div className="form-group row">
                     <label for="example-time-input" className="col-2 col-form-label cal-form-label">Start Time</label>
                     <div className="col-10">
-                        <input className="form-control" type="time" value="13:45:00" id="example-time-input"/>
+                        <input className="form-control" type="time" />
                     </div>
                 </div>
 
                 <div className="form-group row">
                     <label for="example-time-input" className="col-2 col-form-label cal-form-label">End Time</label>
                     <div className="col-10">
-                        <input className="form-control" type="time" value="13:45:00" id="example-time-input"/>
+                        <input className="form-control" type="time" />
                     </div>
                 </div>
              
