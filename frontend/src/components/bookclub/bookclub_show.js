fcc4d62593b1ab358eac418a42535ff01bbad32d
kevinsu <kevin.suboy@gmail.com>
2020-11-11 16:29:27 -0800

    basic styling. fix bookshow container no data on refresh

--- frontend/src/components/bookclub/bookclub_show.js
+++ frontend/src/components/bookclub/bookclub_show.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import BooksFeedContainer from '../profile/books_feed_container';
 
 class BookClubShow extends React.Component {
 
@@ -21,13 +22,14 @@ class BookClubShow extends React.Component {
     render() {
         // debugger
         const { bookclub} = this.props;
-        const books = this.props.books.map((el, i) =>
-            <Link to={`/books/${el._id}`}>
-                <li key={i} className="bookclub-books-list-item">
-                    <h2>{el.title}</h2>
-                </li>
-            </Link>
-        )
+        // const books = this.props.books.map((el, i) =>
+        //     <Link to={`/books/${el._id}`}>
+        //         <li key={i} className="bookclub-books-list-item">
+        //             <h2>{el.title}</h2>
+        //         </li>
+        //     </Link>
+        // )
+        debugger
         const forums = this.props.forums.map((el, i) =>
             <Link to={`/forums/${el._id}`}>
                 <li key={i} className="bookclub-forums-list-item">
@@ -46,13 +48,13 @@ class BookClubShow extends React.Component {
             <div className="bookclub-show-container">
                 <h1>BookClub - </h1>
                 <h2>{bookclub ? bookclub.title:""}</h2>
-                <h3>Books</h3>
-                <ul className="bookclub-books-list">
-                    {books}
-                </ul>
                 <h3>Forums</h3>
                 <ul className="bookclub-forums-list">
                     {forums}
+                </ul>
+                <h3>Books</h3>
+                <ul className="bookclub-books-list">
+                    <BooksFeedContainer />
                 </ul>
                 <h3>Members</h3>
                 <ul className="bookclub-users-list">
--- frontend/src/components/bookclub/bookclub_show_container.js
+++ frontend/src/components/bookclub/bookclub_show_container.js
@@ -4,15 +4,17 @@ import { fetchBookClub } from '../../actions/bookclub_actions';
 import { fetchFilteredBookClubForums, clearForumsFilter, changeForumsFilter } from '../../actions/filters/forum_filter_actions';
 import { clearBooksFilter, changeBooksFilter, fetchFilteredBookClubBooks } from '../../actions/filters/book_filter_actions';
 import { clearUsersFilter, changeUsersFilter, fetchFilteredBookClubUsers } from '../../actions/filters/user_filter_actions';
-import { refSelector } from '../selectors/index_selectors';
+import { refSelector, bookclubShowSelector } from '../selectors/index_selectors';
 
-const mSTP = ({ entities }, { match }) => ({
+const mSTP = ({ entities }, { match }) => {
+    debugger
+    return {
     bookclubId: match.params.bookclubId,
     bookclub: entities.bookclubs[match.params.bookclubId],
-    books: refSelector(entities.bookclubs[match.params.bookclubId].books, entities.books),
-    users: refSelector(entities.bookclubs[match.params.bookclubId].users, entities.users),
-    forums: refSelector(entities.bookclubs[match.params.bookclubId].forums, entities.forums),
-});
+    books: bookclubShowSelector(entities.bookclubs[match.params.bookclubId], entities.books,"books"),
+    users: bookclubShowSelector(entities.bookclubs[match.params.bookclubId], entities.users,"users"),
+    forums: bookclubShowSelector(entities.bookclubs[match.params.bookclubId], entities.forums,"forums"),
+}};
 
 const mDTP = dispatch => ({
     fetchBookClub: (id) => dispatch(fetchBookClub(id)),
