71dbb70b1f49bb70ca2236d4a9fd6eae7d7e0047
AlexArchibeque <wonkman33@gmail.com>
2020-11-17 20:57:10 -0800

    toggleable show

--- frontend/src/components/profile/invites.js
+++ frontend/src/components/profile/invites.js
@@ -4,14 +4,29 @@ class Invites extends React.Component {
     constructor(props){
         super(props)
         this.props = props;
+        this.state = {
+            showInvites: false
+        }
+
+        this.handleOpen = this.handleOpen.bind(this)
+    }
+
+    handleOpen(){
+        this.setState({
+            showInvites: !this.state.showInvites
+        })
     }
 
     render(){
         let {invitesArray } = this.props;
         return(
             <ul>
-                <h1>Top of Invites Arr</h1>
-                <div className="invites-dropdown-li-items hidden">
+                <button onClick={this.handleOpen}>Invites! ( {invitesArray.length} )</button>
+
+                <div 
+                className={`invites-dropdown-li-items 
+                ${this.state.showInvites ? "show" : "hidden"}`}>
+
                 {invitesArray ? invitesArray.map( (invite,idx) => {
                     return(
                         <li className="individual-invite-li-container"> 
