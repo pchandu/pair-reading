563b1fd68b4d5bfe7c808cda8801885d717b6326
AlexArchibeque <wonkman33@gmail.com>
2020-11-23 11:19:34 -0800

    book show all stylings

--- frontend/src/components/books/book_show_all.js
+++ frontend/src/components/books/book_show_all.js
@@ -10,6 +10,18 @@ class BookShowAll extends React.Component {
     this.props.fetchAllBooks();
   }
 
+  showInfo(bookNum){
+    let book = document.getElementById(`text${bookNum}`)
+    book.classList.add("show")
+    book.classList.remove("hidden")
+  }
+
+  hideInfo(bookNum){
+    let book = document.getElementById(`text${bookNum}`)
+    book.classList.add("hidden")
+    book.classList.remove("show")
+  }
+
   render() {
     // if (!this.props.books) return null
     if (!this.props.books) return <div />;
@@ -21,20 +33,32 @@ class BookShowAll extends React.Component {
         <ul className="book-show-all-ul">
           {Object.values(books).map((book, i) => {
             return (
-              <Link className="book-show-all-link" key={i} to={`/books/${book._id}`}>
-                <div className="book-show-all-item-div">
-               
+                <div 
+                
+                onMouseEnter={() => this.showInfo(i)}
+                onMouseLeave={() => this.hideInfo(i)}
+                className="container-for-book-show-item">
+                  <Link className="book-show-all-link" 
+                  key={i} 
+                  to={`/books/${book._id}`}
+                  >
                     <img
                       src={`${book.imagePath}`}
                       className="book-show-all-cover-photo"
                     />
-                    <div className="book-show-all-text">
-                        <p className="book-show-all-title">{book.title}</p>
-                        <p className="book-show-all-author">by {book.author}</p>
-                    </div>
-                
-                </div>
-              </Link>
+                  </Link>
+                  <div className={`book-show-all-text`}
+                  id={`text${i}`}>
+                      <p className="book-show-all-title">{book.title}</p>
+                      <p className="book-show-all-author">{book.author}</p>
+                      <p>{book.description.slice(0,180)}...
+                        {/* {book.description.length > 180 ? 
+                        <button onClick={}>more</button>
+                        : ''} */}
+                      </p>
+                  </div>
+
+               </div>
             );
           })}
         </ul>
