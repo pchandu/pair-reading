48575817d92b4d12cd0613e5cf230c98e66f7a9c
kevinsu <kevin.suboy@gmail.com>
2020-12-01 13:51:45 -0800

    properly update page as delete/new happens

--- frontend/src/components/forum/forum_show.js
+++ frontend/src/components/forum/forum_show.js
@@ -37,19 +37,26 @@ class ForumShow extends React.Component {
             }
         }
     }
-    componentWillUpdate(ownProps,ownState){
-        if(this.state.page !== ownState.page){
-            // debugger
-            this.props.changePostsFilter("offset",(ownState.page-1) * this.postsPerPage);
+    componentWillUpdate(nextProps,nextState){
+        // if(this.props.posts.length !==)
+        const cond1 = this.state.page !== nextState.page; // page change
+        const cond2 = Object.keys(nextProps.posts).length !== Object.keys(this.props.posts).length; // post was deleted or added
+        debugger
+        if(cond1 || cond2){
+            // this.props.fetchForum(this.props.forumId);
+            this.props.changePostsFilter("offset",(nextState.page-1) * this.postsPerPage);
             this.props.fetchFilteredForumPosts(this.props.forumId);
+            if(cond2){
+                this.props.fetchForum(this.props.forumId);
+            }
         }
     }
     
     render() {
         // debugger
-        const l = this.props.forum.posts ? this.props.forum.posts.length : 1;
+        const l = (this.props.forum.posts && this.props.forum.posts.length > 0) ? this.props.forum.posts.length : 1;
         this.maxPage = Math.ceil(l/this.postsPerPage);
-        debugger
+        // debugger
         return (
             <div className="forum-show-container">
                 <h1 className="forum-title">{this.props.forum.title}</h1>
