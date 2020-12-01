f99ed727430cbd3882164415c5b95699922b794a
Kathy Chan <kathykemopaw@gmail.com>
2020-11-30 17:25:49 -0800

    Homepage signup button at the bottom dynamic to change to dashboard button upon already signed in

--- frontend/src/components/homepage/innerParts/front_page_signup.js
+++ frontend/src/components/homepage/innerParts/front_page_signup.js
@@ -5,11 +5,14 @@ import { connect } from 'react-redux';
 
 class FrontPageSignup extends React.Component {
     render(){
-        let signupButton = (this.props.loggedIn) ? <div className="front-page-signup-link-logged-in"></div> : <Link to="/signup" className="front-page-signup-link">Sign Up Now!</Link>;
+        let signupButton = (this.props.loggedIn) ? <Link to="/dashboard" className="front-page-signup-link">Dashboard</Link> : <Link to="/signup" className="front-page-signup-link">Sign Up Now!</Link>;
+        let signupText = (this.props.loggedIn) ? <p>Head to your Dashboard!</p> : <p>Are you ready to begin reading?</p>;
+
         return(
             <div className="front-page-outer-container">
             <div className="front-page-signup-container">
-                <p>Are you ready to begin reading?</p>
+              {signupText}
+                {/* <p>Are you ready to begin reading?</p> */}
                 {/* <Link to="/signup" className="front-page-signup-link">Sign Up Now!</Link> */}
                 {signupButton}
             </div>
